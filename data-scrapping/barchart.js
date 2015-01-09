c8fd62fdaf83d56c4d0ad2f5bf7d6249878961f0
AkhilHector <akhilhotboy@gmail.com>
2015-01-09 12:43:06 +0530

    Unstaged changes

---
+++ data-scrapping/barchart.js
@@ -0,0 +1,32 @@
+/* This graph represents bar chart.Data is externally parsed from the 
+DB. We write a function that processes a HTTP GET request from the 
+requested Url (Json) as the first argument of data.
+Author : AkhilHector
+		X Axis Represents :
+		Y Axis Represents : 
+D3.js : version v3.0
+*/
+	d3.select("body").append("svg").text("Inclusion of tool tip is done in the main page");
+
+var set_margin = {
+		set_top:40 , set_left:40 , set_bottom:40 , set_right:40 
+};
+var set_height = 600 - set_margin.set_top - set_margin.set_bottom;
+var set_width = 1000 - set_margin.set_left - set_margin.set_right;
+var set_x = d3.scale.linear().range([0,set_width]);
+var set_y = d3.scale.linear().range([set_height,0]);
+var set_color = d3.scale.category20();    // Ordianal Scale with 10 categorical colors
+var set_xaxis =  d3.svg.axis().scale(set_x).orient('bottom');
+var set_yaxis =  d3.svg.axis().scale(set_y).orient('left');
+var set_tip = d3.tip()
+				.attr("class" , "tip")
+			
+
+var set_svg = d3.select("body").append("set_svg")
+				.attr("width",set_width + set_margin.set_left + set_margin.set_right)
+				.attr("height",set_height + set_margin.set_top + set_margin.set_left)
+			.append("svg")
+				.attr("transform", "translate(" + set_margin.set_left + "," + set_margin.set_top + ")");
+				
+
+				
