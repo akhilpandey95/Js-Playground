79dfb12a74c9adfd76c3480ac113f4981f5800c5
AkhilHector <akhilhector.1@gmail.com>
2015-08-11 23:42:06 +0530

    Associative arrays and first occurence made, & removed all ';'

--- misc/listcomp.js
+++ misc/listcomp.js
@@ -5,36 +5,39 @@ var fs = require('fs')
 
 function list_comp() {
         var arr = [],
-            arr1 = [];
-        arr["one"] = "The value in index one";
-        arr["two"] = "The value in index two";
-        var data = fs.readFileSync("p.txt", "utf8");
-        var str = data.split(" ");
+            arr1 = []
+        arr["one"] = "The value in index one"
+        arr["two"] = "The value in index two"
+        var data = fs.readFileSync("p.txt", "utf8")
+        var str = data.split(" ")
         for(x=0; x<str.length; x++) {
-                var index = "w";
-                var val = index + x;
-                arr1[val] = str[x];
+                var val = ("w").concat(x)
+                arr1[val] = str[x]
         }
-        console.log(arr1);
+        console.log(arr1)
 }
 
-function list_first_occur() {
+function list_first_occur(args) {
         var arr = [],
             data = fs.readFileSync("p.txt", "utf8"),
-            str = data.split(" ");
+            str = data.split(" ")
         for(x=0; x<str.length; ++x) {
-                var ind = "w",
-                    val = ind + x;
-                arr[val] = str[x];
+                var val = ("w").concat(x)
+                arr[val] = str[x]
         }
-        var q1 = arr.search("for");
-        console.log(q1);
+        for(x in arr) {
+                if(arr[x] === args) {
+                        console.log(x)
+                }
+        }
 }
 
 function main() {
-        console.log("list comprehensions and associative arrays");
-        list_comp();
-        list_first_occur();
+        console.log("list comprehensions and associative arrays")
+        list_comp()
+        list_first_occur("programming") // would pipe out ass index of programming
+        list_first_occur("algorithms") // would pipe out ass index of algorithms
+        list_first_occur("data") // would pipe out ass index of data
 }
 
-main();
+main()
